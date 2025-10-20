4ba5a696d8f4d4e91199c349d79f81605838a2b6
Baraa227 <baraad567@gmail.com>
2025-10-20 12:58:15 +0200

    Skapa larm
    Väljer man detta alternativ får man upp ytterligare en meny där man får välja att konfigurera larm inom tre områden eller gå tillbaka till huvudmenyn.

--- alarms.py
+++ alarms.py
@@ -1,1 +1,43 @@
+def create_alarms(alarms_list):
+    while True:
+        print("\n-- Skapa larm--")
+        print("1. CPU användning")
+        print("2. Minnesanvändning")
+        print("3. Diskanvändning")
+        print("0. Tillbaka till huvudmenyn")
 
+        choice = input("Välj ett alternativ: ")
+
+        if choice == "0":
+            print("Återgår till huvudmenyn ")
+            break
+
+        alarm_type = None
+        if choice == "1":
+            alarm_type = "CPU"
+        elif choice == "2":
+            alarm_type = "Minne"
+        elif choice == "3":
+            alarm_type == "Disk"
+        else:
+            print("Fel val, försök igen! ")
+            continue
+
+        level = input(f"Ställ in nivå för {alarm_type}-larm (0-100): ")
+
+        if not level.isdigit():
+            print("Fel: Du måste ange en siffra! ")
+            continue
+
+        level = int(level)
+        if level < 1 or level > 100:
+            print("Fel: Nivån måste vara mellan 0 och 100! ")
+            continue
+
+        alarms_list.append({"typ": alarm_type, "nivå": level})
+        print(f"Larm för {alarm_type} användning satt till {level}%")
+
+        break
+print()
+
+    
